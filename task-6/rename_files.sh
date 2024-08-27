b67ea58e40d0bc126c7cc89a64413243442c5815
omargamal <muomiga925@gmail.com>
2024-08-27 19:05:08 +0300

    will be send

--- task-6/rename_files.sh
+++ task-6/rename_files.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-for file in *.txt;
-do
-	name=$file
-	name="old_$name"
-	mv $file $name
-done
+echo "what is the path?"
+read path
+if [[ -d "$path" ]]
+        then
+                cd "$path"
+                for file in *.txt;
+                do
+                name=$file
+                name="old_$name"
+                mv $file $name
+                done
+else
+        echo "Wrong Path"
+fi
+
